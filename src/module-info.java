5251648d66410827cd768f49f141d6d784446230
Joseba <117350714+Josebasmh@users.noreply.github.com>
2024-01-17 10:14:57 +0100

    Squashed commit of the following:
    commit cfe473f94b6154ffc98df54245c888f9d9360ed2
    Author: Joseba <117350714+Josebasmh@users.noreply.github.com>
    Date:   Wed Jan 17 10:14:45 2024 +0100
    
        ADD-Estructura básica
    
        Añadida la estructura básica y librerías necesarias

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,19 @@
  * 
  */
 module Examen_1_Recupaeracion {
+	requires javafx.controls;
+	requires java.desktop;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires java.sql;
+	
+	opens model to javafx.graphics, javafx.fxml, javafx.base;
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+	opens controller to javafx.graphics, javafx.fxml, javafx.base;
+	opens dao to javafx.base,java.sql;
+	opens conexion to java.sql;
 }
