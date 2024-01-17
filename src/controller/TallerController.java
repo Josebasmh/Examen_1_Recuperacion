141845ae6c0168f73688be7c960ffcc7d948e627
Joseba <117350714+Josebasmh@users.noreply.github.com>
2024-01-17 11:31:33 +0100

    Squashed commit of the following:
    commit 9b384981a028c7014165f23e8672485c0e23251e
    Author: Joseba <117350714+Josebasmh@users.noreply.github.com>
    Date:   Wed Jan 17 11:31:24 2024 +0100
    
        MOD-fxml

--- src/controller/TallerController.java
+++ src/controller/TallerController.java
@@ -13,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
