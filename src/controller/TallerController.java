6c2b09165ce7cea10b03fa3db49877d18b7d8751
Joseba <117350714+Josebasmh@users.noreply.github.com>
2024-01-17 10:21:00 +0100

    ADD-fxml
    Añadido el fxml con su respectivo controlador.

--- src/controller/TallerController.java
+++ src/controller/TallerController.java
@@ -1,5 +1,62 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class TallerController {
+
+    @FXML
+    private Button btnActualizar;
+
+    @FXML
+    private Button btnCrear;
+
+    @FXML
+    private CheckBox cbDisponible;
+
+    @FXML
+    private TableColumn<?, ?> tcCodigo;
+
+    @FXML
+    private TableColumn<?, ?> tcDisponible;
+
+    @FXML
+    private TableColumn<?, ?> tcNombre;
+
+    @FXML
+    private TableColumn<?, ?> tcPrecio;
+
+    @FXML
+    private TextField tfCodigo;
+
+    @FXML
+    private TextField tfNombre;
+
+    @FXML
+    private TextField tfPrecio;
+
+    @FXML
+    private TableView<?> tvTabla;
+
+    @FXML
+    void actualizarProducto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crearProducto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarProducto(MouseEvent event) {
+
+    }
 
 }
